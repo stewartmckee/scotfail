03aa6902141ce60845b475e90f23c88c7fe8cb76
Stewart McKee <stewart@theizone.co.uk>
2012-11-06 16:30:36 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scotfail::Application.config.secret_token = 'd84dcc5958d57fa3cfa8f53e8218accb0b0a626d1d13504aaffbf2bc11af164b86a65ca8753f813d8c91efe0f2ef5936d81258e25c8123da36008feba1164995'
