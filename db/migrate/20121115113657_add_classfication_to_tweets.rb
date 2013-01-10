0b3c004d3336c44abfc3e33508c8376c93d1efa4
Stewart McKee <stewart@theizone.co.uk>
2013-01-10 22:28:35 +0000

    updated db migration

--- db/migrate/20121115113657_add_classfication_to_tweets.rb
+++ db/migrate/20121115113657_add_classfication_to_tweets.rb
@@ -1,6 +1,6 @@
-class AddClassficationToTweets < ActiveRecord::Migration
+ class AddClassficationToTweets < ActiveRecord::Migration
   def up
-    #add_column :tweets, :classification, :string
+    add_column :tweets, :classification, :string
     
     Tweet.all.each do |tweet|
       tweet.update_attributes(:classification => tweet.classify)
